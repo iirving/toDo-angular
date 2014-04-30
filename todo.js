67063937258e7716e3768e06713baf71abb71708
Ian Irving <ian@falsepositives.com>
2014-04-30 19:06:13 -0400

    add function to get total todos dynamical

--- todo.js
+++ todo.js
@@ -1,12 +1,20 @@
 function TodoCtrl ($scope) {
-  $scope.TotalTodos = 4;
+
 
 $scope.todos = [{text: 'learn AngularJS', done:false},
                 {text: 'Build an App', done:false }
             ];
 
+
+$scope.getTotalTodos = function () {
+  return $scope.todos.length;
+}
+
+
+
 $scope.addTodo = function () {
   $scope.todos.push({text:$scope.formTodoText, done:false});
+  $scope.formTodoText = '';
 };
 
 }
