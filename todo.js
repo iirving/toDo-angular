52704ad3be66e8482657c438f0e8e395159fa511
Ian Irving <ian@falsepositives.com>
2014-04-30 19:00:06 -0400

    first try at add to Do function

--- todo.js
+++ todo.js
@@ -5,5 +5,8 @@ $scope.todos = [{text: 'learn AngularJS', done:false},
                 {text: 'Build an App', done:false }
             ];
 
+$scope.addTodo = function () {
+  $scope.todos.push({text:$scope.formTodoText, done:false});
+};
 
 }
