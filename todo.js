1a04fbe834090101e3e8ae7b2159053c734feb48
Ian Irving <ian@falsepositives.com>
2014-04-30 19:15:47 -0400

    add function to remove complete todo items

--- todo.js
+++ todo.js
@@ -8,9 +8,13 @@ $scope.todos = [{text: 'learn AngularJS', done:false},
 
 $scope.getTotalTodos = function () {
   return $scope.todos.length;
-}
+};
 
-
+$scope.removeCompleted = function () {
+  $scope.todos = _.filter($scope.todos, function(todo){
+    return !todo.done;
+  })
+};
 
 $scope.addTodo = function () {
   $scope.todos.push({text:$scope.formTodoText, done:false});
