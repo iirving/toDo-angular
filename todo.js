785bfca0795bbed384b8c25f2e03625c8c6f4619
Ian Irving <ian@falsepositives.com>
2014-04-30 18:48:43 -0400

    initial add of files

---
+++ todo.js
@@ -0,0 +1,9 @@
+function TodoCtrl ($scope) {
+  $scope.TotalTodos = 4;
+
+$scope.todos = [{text: 'learn AngularJS', done:false},
+                {text: 'Build an App', done:false }
+            ];
+
+
+}
